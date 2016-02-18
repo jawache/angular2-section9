f0716054a54b9d8735904ebf1f25cecd4e48a0bd
Asim Hussain <jawache@gmail.com>
2016-02-18 12:35:07 +0000

    Bootstrapped

--- completed/app.ts
+++ completed/app.ts
@@ -1,5 +1,11 @@
  import {UpgradeAdapter} from 'angular2/upgrade';
- const upgradeAdapter = new UpgradeAdapter();
+
+ //declare var angular:any;
+
+ const upgradeAdapter: UpgradeAdapter = new UpgradeAdapter();
+
+ //angular.module('angular-legacy').directive('algorithms',adapter.downgradeNg2Component(Algorithms));
+
  upgradeAdapter.bootstrap(document.body, ['codecraft'], {strictDi: false});
 
 
